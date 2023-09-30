47123785ac1839295866e576dafaab27fa4fea33
Home <zhurukhin.nikita@gmail.com>
2023-09-30 16:29:47 +0300

    изменения 1

--- Source.cpp
+++ Source.cpp
@@ -4,13 +4,13 @@ int main() {
 	setlocale (0, "");
 	cout << "int: " << sizeof(int) << "  short int: " << sizeof(short int) << "  float: " << sizeof(float) << "  double: " << sizeof(double) << "  long double: " << sizeof(long double) << "  char: " << sizeof(char) << "  bool: " << sizeof(bool);
 	cout << "\n";
-	cout << "Введите целое число, двоичное представление которого хотите получить: ";
+	cout << "Enter the integer whose binary representation you want to get: ";
 	int a;
 	cin >> a;
 	cout << "\n";
 	int order = sizeof(int) * 8;
 	unsigned int mask = 1 << order - 1;
-	cout << "a:" << " " << a << " " << "место в памяти" << " " << '-' << " " << &a << " " << "mask:" << " " << mask << " " << "\n";
+	cout << "a:" << " " << a << " " << "place in memory" << " " << '-' << " " << &a << " " << "mask:" << " " << mask << " " << "\n";
 	for (int i = 1; i <= order; i++)
 	{
 		putchar(a & mask ? '1' : '0');
@@ -29,11 +29,11 @@ int main() {
 		float floatB;
 	};
 	cout << "\n";
-	cout << "Введите число типа float, двоичное представление которого хотите получить: ";
+	cout << "Enter the float type number whose binary representation you want to get: ";
 	cin >> floatB;
 	mask = 1 << order - 1;
 	cout << "\n";
-	cout << "integerA:" << " " << integerA << " " << "место в памяти" << " " << '-' << " " << &integerA << " " << "floatB:" << " " << floatB << " " << "место в памяти" << " " << '-' << " " << &floatB << "\n";
+	cout << "integerA:" << " " << integerA << " " << "place in memory" << " " << '-' << " " << &integerA << " " << "floatB:" << " " << floatB << " " << "place in memory" << " " << '-' << " " << &floatB << "\n";
 	for (int i = 1; i <= order; i++)
 	{
 		putchar(integerA & mask ? '1' : '0');
@@ -52,10 +52,10 @@ int main() {
 		double doubleC /*= 1.2345e+2*/;
 	};
 	cout << "\n";
-	cout << "Введите число типа double, двоичное представление которого хотите получить: ";
+	cout << "Enter a number of type double whose binary representation you want to get: ";
 	cin >> doubleC;
 	cout << "\n";
-	cout << "A:" << " " << "{" << A[0] << "," << A[1] << "}" << " " << "место в памяти" << " " << "-" << " " << &A << " " << "doubleC:" << " " << doubleC << " " << "место в памяти" << " " << "-" << " " << &doubleC << "\n";
+	cout << "A:" << " " << "{" << A[0] << "," << A[1] << "}" << " " << "place in memory" << " " << "-" << " " << &A << " " << "doubleC:" << " " << doubleC << " " << "place in memory" << " " << "-" << " " << &doubleC << "\n";
 	int order2 = sizeof(double) * 8;
 	//cout << A << " " << order2 << " " << &A << endl;
 	for (int j = 1; j >= 0; j--)
